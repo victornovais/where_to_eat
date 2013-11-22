60da4a57a09d96399835727f401ae694865cdb37
VictorNovais <victor.novais@sieve.com.br>
2013-11-22 14:46:19 -0200

    Primeira versão do algoritimo.

---
+++ restaurants.py
@@ -0,0 +1,71 @@
+# -*- coding:utf-8 -*-
+approved_restaurants = [
+    {
+        'name': 'Alma Carioca',
+        'expensive': True
+    },
+    {
+        'name': 'Sociedade Secreta',
+        'expensive': False
+    },
+    {
+        'name': 'Parmê',
+        'expensive': False
+    },
+    {
+        'name': 'Montana Grill',
+        'expensive': True
+    },
+    {
+        'name': 'Otime',
+        'expensive': False
+    },
+    {
+        'name': 'Mexicano',
+        'expensive': False
+    },
+    {
+        'name': 'Gourmet',
+        'expensive': True
+    },
+    {
+        'name': 'As meninas',
+        'expensive': False
+    },
+    {
+        'name': 'Imperial',
+        'expensive': False
+    },
+    {
+        'name': 'Colarinho',
+        'expensive': True
+    },
+    {
+        'name': 'Versão Carioca',
+        'expensive': True
+    },
+    {
+        'name': 'Boteco 98',
+        'expensive': False
+    },
+    {
+        'name': 'KFC',
+        'expensive': False
+    },
+    {
+        'name': 'Planeta Grill',
+        'expensive': False
+    },
+    {
+        'name': 'Atrium',
+        'expensive': False
+    },
+    {
+        'name': 'Sabor & Recheio',
+        'expensive': False
+    },
+    {
+        'name': 'Chopp Time',
+        'expensive': True
+    }
+]
---
+++ where_to_eat.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from restaurants import approved_restaurants as my_restaurants
+from random import choice
+
+
+def where_to_eat():
+    chosen_one = choice(my_restaurants)
+    print chosen_one['name']
+
+
+
+if __name__ == '__main__':
+    where_to_eat()
+
